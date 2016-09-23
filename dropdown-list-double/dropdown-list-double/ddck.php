8cae0da34e725773f5b93103918984471937084a
Aishwarya-351 <aishwaryap@cybage.com>
2016-09-23 20:10:30 +0530

    Add files via upload

---
+++ dropdown-list-double/dropdown-list-double/ddck.php
@@ -0,0 +1,21 @@
+/* //Back end PHP script to collect matching options for second list. Returns data as JSON string */
+
+<?Php
+@$cat_id=$_GET['cat_id'];
+//$cat_id=2;
+/// Preventing injection attack //// 
+if(!is_numeric($cat_id)){
+echo "Data Error";
+exit;
+ }
+/// end of checking injection attack ////
+require "config.php";
+
+$sql="select subcategory,subcat_id from subcategory where cat_id='$cat_id'";
+$row=$dbo->prepare($sql);
+$row->execute();
+$result=$row->fetchAll(PDO::FETCH_ASSOC);
+
+$main = array('data'=>$result);
+echo json_encode($main);
+?>
